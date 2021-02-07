a6d219d77e86625f0fcefc0f4ba2c97342ed5e89
dpavlov <dpavlov@iitdgroup.ru>
2021-02-07 17:20:15 +0700

    create Kotlin multiplatform library

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    kotlin("multiplatform") version "1.4.30"
+}
+
+group = "me.denis"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+    }
+    js(LEGACY) {
+        browser {
+
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+
+        val jvmMain by getting
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+            }
+        }
+
+        val jsMain by getting
+        val jsTest by getting {
+            dependencies {
+                implementation(kotlin("test-js"))
+            }
+        }
+    }
+}
